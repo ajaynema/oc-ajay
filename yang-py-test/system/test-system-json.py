681ece7af54992cdefc9c2fa5351e9e618dc07af
ajaynema <ajay.nema@gmail.com>
2021-07-15 16:44:55 +0530

    added system load from json

---
+++ yang-py-test/system/test-system-json.py
@@ -0,0 +1,11 @@
+from __future__ import print_function, unicode_literals
+
+import os
+import sys, os.path
+YANG_PY_DIR = "../../yang-py"
+sys.path.append(YANG_PY_DIR)
+sys.path.append(".")
+import system 
+import pyangbind.lib.pybindJSON as pybindJSON
+new_ocs = pybindJSON.load(os.path.join(".", "system.json"), system, "openconfig_system")
+print(pybindJSON.dumps(new_ocs))
