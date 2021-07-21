9ed8d766cfea5f6cd32e176cad017afa63dc4c42
ajaynema <ajay.nema@gmail.com>
2021-07-21 13:19:57 +0530

    added read me file

--- netconf-client/test/test-get-running-config.py
+++ netconf-client/test/test-get-running-config.py
@@ -1,7 +1,7 @@
 from ncclient import manager
 
-m = manager.connect(host='localhost', port='17830', username='admin',
-                    password='netconf', device_params={'name':'iosxe'}, hostkey_verify=False)
+m = manager.connect(host='localhost', port='11002', username='root',
+                    password='root', device_params={'name':'iosxe'}, hostkey_verify=False)
 
 print(m.connected)
 reply = m.get_config('running')
