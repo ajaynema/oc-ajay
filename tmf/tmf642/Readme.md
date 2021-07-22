cf82aaf89bdfaeefb18512fa64c1c3a47ff6e7a2
ajaynema <ajay.nema@gmail.com>
2021-07-22 14:33:17 +0530

    added apis

--- tmf/tmf642/Readme.md
+++ tmf/tmf642/Readme.md
@@ -14,5 +14,21 @@
  => Define TMF perceieved severity - Done
  => Define TMF Alarm type - Done
  => Alarm APIs End points
+ 
+        Single Alarm Operation
+        - Create Alarm - "/alarm",methods='POST'
+        - Modify Alarm - "/alarm/<alarmId>",methods='PATCH'
+        - Delete/Clear Alarm - "/alarm/<alarmId>/clear", methods='POST'
+        - Get Alarm - "/alarm/<alarmId>",methods='GET'
+
+        Multiple Alarms Operations
+        - Get Alarms - "/alarms",methods='GET'
+        - Ack Alarms - "/ackAlarms",methods='POST'
+        - Un Ack Alarms - "/unAckAlarms", methods='POST'
+        - Clear Alarms - "/clearAlarms", methods='POST'
+        - Comment Alarm - "/commentsAlarms"methods='POST'
+        - Group Alarm - "/groupAlarms",methods='POST'
+        - Un Group Alarm - "/ungroupAlarms",methods='POST'
+     
 
  
--- tmf/tmf642/alarm_web_service.py
+++ tmf/tmf642/alarm_web_service.py
@@ -4,97 +4,57 @@ from json import JSONEncoder
 import json
 
 class AlarmWebService:
-    def __init__(self,name):
+    def __init__(self,name="AlarmWebService",port=80):
         self.name = name
-        self.api = Flask(self.name)
-        self.api.add_url_rule("/<resource_name>","get",self.get,methods=['GET'])
-        self.api.add_url_rule("/<resource_name>/<id>","get_by_id",self.getById,methods=['GET'])
-        self.api.add_url_rule("/<resource_name>","add",self.add,methods=['POST'])
-        self.api.add_url_rule("/<resource_name>/<id>","modify",self.modify,methods=['POST'])
-        self.api.add_url_rule("/<resource_name>/<id>/<action>","action",self.action,methods=['POST'])
-        self.api.add_url_rule("/<resource_name>/<id>/<action>","action",self.action,methods=['GET'])
-        self.api.add_url_rule("/<resource_name>/<id>","delete",self.delete,methods=['DELETE'])
+        self.api = Flask(self.name)  
+        #Single Alarm Operations
+        self.api.add_url_rule("/alarm","create_alarm",self.create_alarm,methods=['POST'])
+        self.api.add_url_rule("/alarm/<alarmId>","modify_alarm",self.modify_alarm,methods=['PATCH'])
+        self.api.add_url_rule("/alarm/<alarmId>/clear","clear_alarm",self.modify_alarm,methods=['POST'])
+        self.api.add_url_rule("/alarm/<alarmId>","get_alarm",self.get_alarm,methods=['GET'])
+        #Multiple Alarms Operations
+        self.api.add_url_rule("/alarms","get_alarms",self.get_alarms,methods=['GET'])
+        self.api.add_url_rule("/ackAlarms","ack_alarms",self.ack_alarms,methods=['POST'])
+        self.api.add_url_rule("/unAckAlarms","unack_alarms",self.unack_alarms,methods=['POST'])
+        self.api.add_url_rule("/clearAlarms","clear_alarms",self.clear_alarms,methods=['POST'])
+        self.api.add_url_rule("/commentsAlarms","comment_alarms",self.comment_alarms,methods=['POST'])
+        self.api.add_url_rule("/groupAlarms","group_alarms",self.group_alarms,methods=['POST'])
+        self.api.add_url_rule("/ungroupAlarms","ungroup_alarms",self.ungroup_alarms,methods=['POST'])
+        self.port = port
 
-    def get(self,resource_name):
-        return Response("", 200,mimetype='application/json')
-  
-    def getById(self,resource_name,id):
-        result = {}
-        return Response("", 200,mimetype='application/json')
+    def create_alarm(self):
+        return
+
+    def modify_alarm(self, alarmId):
+        return
+
+    def clear_alarm(self, alarmId):
+        return
     
-    def add(self,resource_name):
-        result = {}
-        resource = self.resourceManager.get(resource_name)
-        if (resource is None):
-            result['code'] = 405
-            result['message'] = "METHOD-NOT-ALLOWED"
-            response_str = jsonpickle.encode(result)
-            return Response(response_str, 405)
-        print("add -> " + resource_name)
-        data = request.get_json()
-        dbContext = DBContext(resource)
-        resultResource = DBManager.insert(dbContext,data)
-        result = {}
-        result['code'] = 0
-        result['message'] = 0
-        result['resource'] = resultResource
-        response_str = jsonpickle.encode(result)
-        return Response(response_str, 200,mimetype='application/json')
+    def get_alarm(self, alarmId):
+        return
 
-    def modify(self,resource_name,id):
-        result = {}
-        resource = self.resourceManager.get(resource_name)
-        if (resource is None):
-            result['code'] = 405
-            result['message'] = "METHOD-NOT-ALLOWED"
-            response_str = jsonpickle.encode(result)
-            return Response(response_str, 405)
-        print("Modify ID -> " + resource_name + ",ID="+id)
-        resource = self.resourceManager.get(resource_name)
-        if (resource is None):
-            result['code'] = 405
-            result['message'] = "METHOD-NOT-ALLOWED"
-            response_str = jsonpickle.encode(result)
-            return Response(response_str, 405)
-        print("add -> " + resource_name)
-        data = request.get_json()
-        dbContext = DBContext(resource)
-        resultResource = DBManager.update(dbContext,id,data)   
-        result['code'] = 0
-        result['message'] = 0
-        result['resource'] = resultResource
-        response_str = jsonpickle.encode(result)
-        return Response(response_str, 200,mimetype='application/json')
-  
-    def delete(self,resource_name,id):
-        print("Delete  -> " + resource_name + ", ID="+id)
-        result = {}
-        resource = self.resourceManager.get(resource_name)
-        if (resource is None):
-            result['code'] = 405
-            result['message'] = "METHOD-NOT-ALLOWED"
-            response_str = jsonpickle.encode(result)
-            return Response(response_str, 405)
-        
-        dbContext = DBContext(resource)
-        resultResource = DBManager.delete(dbContext,id)
-        result['code'] = 0
-        result['message'] = 0
-        result['resource'] = resultResource
-        response_str = jsonpickle.encode(result)
-        return Response(response_str, 200,mimetype='application/json')
+    def get_alarms(self):
+        return
+
+    def ack_alarms(self):
+        return
+
+    def unack_alarms(self):
+        return
+
+    def comments_alarms(self):
+        return
+
+    def ungroup_alarms(self):
+        return
     
-    def action(self,resource_name,id,action):
-        result = {}
-        resource = self.resourceManager.get(resource_name)
-        if (resource is None):
-            result['code'] = 405
-            result['message'] = "METHOD-NOT-ALLOWED"
-            response_str = jsonpickle.encode(result)
-            return Response(response_str, 405)
-        print("Action -> " + resource_name + ", ID="+id + ", Action="+action)
-        return Response('success', 200,mimetype='application/json')
-
+    def group_alarms(self):
+        return
 
     def start(self):
-        self.api.run()
+        self.api.run(host='0.0.0.0', port=self.port)
+
+if __name__ == '__main__':
+      webserice = AlarmWebService()
+      webserice.start()
