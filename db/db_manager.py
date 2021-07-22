d8dd8ec41c6a8ab6540d21a78baf77809f7f9a04
ajaynema <ajay.nema@gmail.com>
2021-07-22 23:16:09 +0530

    Implemenetd many APIs

--- db/db_manager.py
+++ db/db_manager.py
@@ -1,7 +1,12 @@
 from pymongo import MongoClient
 import pymongo
+import uuid
 
 class DbManager:
+
+    @staticmethod
+    def get_unique_id():
+        return str(uuid.uuid4())
 
     @staticmethod
     def get_connection():
@@ -11,13 +16,16 @@ class DbManager:
     
     @staticmethod
     def insert(database,table,data):
+        if "id" not in data:
+            data['id'] = DbManager.get_unique_id()
         data['_id'] = data['id']
         client = DbManager.get_connection()
         db = client[database]
         collection = db[table]
         collection.insert_one(data)
         client.close()
-        return
+        row = DbManager.get_by_id(database,table,data['id'])
+        return row
 
     @staticmethod
     def insert_many(database,table,list):
@@ -39,12 +47,41 @@ class DbManager:
         return
 
     @staticmethod
+    def delete(database,table,id):
+        client = DbManager.get_connection()
+        db = client[database]
+        collection = db[table]
+        myquery = { "_id": id }
+        collection.delete_one(myquery)
+        return
+
+    @staticmethod
     def get_by_id(database,table,id):
         client = DbManager.get_connection()
         db = client[database]
         collection = db[table]
         myquery = { "_id": id }
         data = collection.find_one(myquery)
+        return data
+    
+    @staticmethod
+    def get_all(database,table):
+        client = DbManager.get_connection()
+        db = client[database]
+        collection = db[table]
+        myquery = {}
+        rows = collection.find(myquery)
+        data = []
+        for row in rows:
+          data.append(row)
+        return data
+
+    @staticmethod
+    def query(database,table,query):
+        client = DbManager.get_connection()
+        db = client[database]
+        collection = db[table]
+        data = collection.find_one(query)
         return data
 
     
--- tmf/tmf642/alarm_web_service.py
+++ tmf/tmf642/alarm_web_service.py
@@ -1,44 +1,73 @@
+import sys, os.path
+
+sys.path.append("./../../db")
+
 from flask import Flask,Response,request
 import jsonpickle
-from json import JSONEncoder
-import json
-
+from db_manager import DbManager
+BASE_URL="/tmf-api/alarmManagement/<version>"
 class AlarmWebService:
     def __init__(self,name="AlarmWebService",port=80):
         self.name = name
         self.api = Flask(self.name)  
         #Single Alarm Operations
-        self.api.add_url_rule("/api/<version>/alarm","create_alarm",self.create_alarm,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/alarm/<alarmId>","modify_alarm",self.modify_alarm,methods=['PATCH'])
-        self.api.add_url_rule("/api/<version>/alarm/<alarmId>/clear","clear_alarm",self.modify_alarm,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/alarm/<alarmId>","get_alarm",self.get_alarm,methods=['GET'])
+        self.api.add_url_rule(BASE_URL+"/alarm","create_alarm",self.create_alarm,methods=['POST'])
+        self.api.add_url_rule(BASE_URL+"/alarm/<alarmId>","modify_alarm",self.modify_alarm,methods=['PATCH'])
+        self.api.add_url_rule(BASE_URL+"/alarm/<alarmId>","delete_alarm",self.delete_alarm,methods=['DELETE'])
+        self.api.add_url_rule(BASE_URL+"/alarm/<alarmId>","get_alarm",self.get_alarm,methods=['GET'])
         #Multiple Alarms Operations
-        self.api.add_url_rule("/api/<version>/alarms","get_alarms",self.get_alarms,methods=['GET'])
-        self.api.add_url_rule("/api/<version>/ackAlarms","ack_alarms",self.ack_alarms,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/unAckAlarms","unack_alarms",self.unack_alarms,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/clearAlarms","clear_alarms",self.clear_alarms,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/commentsAlarms","comment_alarms",self.comment_alarms,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/groupAlarms","group_alarms",self.group_alarms,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/ungroupAlarms","ungroup_alarms",self.ungroup_alarms,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/hub","subscribe",self.subscribe,methods=['POST'])
-        self.api.add_url_rule("/api/<version>/hub/<id>","hub",self.unsubscribe,methods=['DELETE'])
+        self.api.add_url_rule(BASE_URL+"/alarms","get_alarms",self.get_alarms,methods=['GET'])
+        self.api.add_url_rule(BASE_URL+"/alarm","get_alarms",self.get_alarms,methods=['GET'])
+        self.api.add_url_rule(BASE_URL+"/ackAlarms","ack_alarms",self.ack_alarms,methods=['POST'])
+        self.api.add_url_rule(BASE_URL+"/unAckAlarms","unack_alarms",self.unack_alarms,methods=['POST'])
+        self.api.add_url_rule(BASE_URL+"/clearAlarms","clear_alarms",self.clear_alarms,methods=['POST'])
+        self.api.add_url_rule(BASE_URL+"/commentsAlarms","comment_alarms",self.comment_alarms,methods=['POST'])
+        self.api.add_url_rule(BASE_URL+"/groupAlarms","group_alarms",self.group_alarms,methods=['POST'])
+        self.api.add_url_rule(BASE_URL+"/ungroupAlarms","ungroup_alarms",self.ungroup_alarms,methods=['POST'])
+        self.api.add_url_rule(BASE_URL+"/hub","subscribe",self.subscribe,methods=['POST'])
+        self.api.add_url_rule(BASE_URL+"/hub/<id>","unsubscribe",self.unsubscribe,methods=['DELETE'])
         self.port = port
 
     def create_alarm(self,version):
-        return
+        data = request.get_json()
+        if ("alarmType" not in data):
+            return Response("", 409 ,mimetype='application/json')
+        if ("perceivedSeverity" not in data):
+            return Response("", 409 ,mimetype='application/json')
+        if ("alarmedObject" not in data):
+            return Response("", 409 ,mimetype='application/json')
+        if ("sourceSystemId" not in data):
+            return Response("", 409 ,mimetype='application/json')
+        if ("probableCause" not in data):
+            return Response("", 409 ,mimetype='application/json')
+        if ("alarmRaisedTime" not in data):
+            return Response("", 409 ,mimetype='application/json')
+        row = DbManager.insert("DB_ALARM","alarm",data)
+        response_str = jsonpickle.encode(row)
+        return Response(response_str, 200, mimetype='application/json')
 
     def modify_alarm(self,version,alarmId):
-        return
-
-    def clear_alarm(self,version,alarmId):
-        return
+       return
+ 
+    def delete_alarm(self,version,alarmId):
+        query = {"id" : alarmId}
+        row = DbManager.query("DB_ALARM","alarm",query)
+        if (row is None):
+            return Response("", 404 ,mimetype='application/json')
+        DbManager.delete("DB_ALARM","alarm",alarmId)
+        return Response("", 204, mimetype='application/json')
     
     def get_alarm(self,version,alarmId):
-        return
+        query = {"id" : alarmId}
+        row = DbManager.query("DB_ALARM","alarm",query)
+        response_str = jsonpickle.encode(row)
+        return Response(response_str, 200, mimetype='application/json')
 
     def get_alarms(self,version):
-        return
-
+        row = DbManager.get_all("DB_ALARM","alarm")
+        response_str = jsonpickle.encode(row)
+        return Response(response_str, 200, mimetype='application/json')
+   
     def ack_alarms(self,version):
         return
 
@@ -58,9 +87,27 @@ class AlarmWebService:
         return
 
     def subscribe(self,version):
-        return
+        data = request.get_json()
+        callback = data['callback']
+        query = {"callback" : callback}
+        row = DbManager.query("DB_PROFILE","alarm_subscription",query)
+        if (row is not None):
+            return Response("", 409 ,mimetype='application/json')
+        row = DbManager.insert("DB_PROFILE","alarm_subscription",data)
+        result={}
+        result['id'] = row['id']
+        result['callback'] = row['callback']
+        response_str = jsonpickle.encode(result)
+        print(response_str)
+        return Response(response_str, 200, mimetype='application/json')
+
     def unsubscribe(self,version,id):
-        return
+        query = {"id" : id}
+        row = DbManager.query("DB_PROFILE","alarm_subscription",query)
+        if (row is None):
+            return Response("", 404 ,mimetype='application/json')
+        DbManager.delete("DB_PROFILE","alarm_subscription",id)
+        return Response("", 204, mimetype='application/json')
 
     def start(self):
         self.api.run(host='0.0.0.0', port=self.port)
